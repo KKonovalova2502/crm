428bbe1891cfdd4e235c6f1b5bf53fd30f6f91b4
KKonovalova2502 <k.konovalova2502@gmail.com>
2025-10-24 22:41:21 +0300

    studied Static and Dynamic Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from '@/app/components/status-label';
 
-export default function Home() {
+export default async function Home() {
+  const headerList = await headers();
+  console.log('User-Agent:', headerList.get('user-agent'));
+  // Не логай весь headerList напряму
+
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
