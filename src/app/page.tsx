296457b669a9bce48d80986f91af18b5585827c0
KKonovalova2502 <k.konovalova2502@gmail.com>
2025-11-06 18:01:36 +0200

    practice with based routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { headers } from 'next/headers';
 import StatusLabel, { Status } from '@/app/components/status-label';
 import AddCompanyButton from '@/app/components/add-company-button';
 
 export default async function Home() {
-  const headerList = await headers();
-  console.log('User-Agent:', headerList.get('user-agent'));
-  // Не логай весь headerList напряму
-
   return (
     <main>
       <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
